f0ce010c404f19b25402e07517dd1a01b5532f8b
Roy Tsabari <a300600@gmail.com>
2014-06-16 15:03:42 +0300

    force js code style

--- tasks/test.js
+++ tasks/test.js
@@ -2,12 +2,13 @@ console.log('TEST amelia-ui ...');
 
 var src = require('./src');
 var docs = require('./docs');
-
+var jsBeautify = require('./staticAnalysis/jsbeautify.js');
 var path = require('path');
 var Q = require('q');
 
 Q.all([
   //src.js.test(),
+  jsBeautify.verifyOnly(),
   docs.js.test()
 ])
   .then(function() {});
