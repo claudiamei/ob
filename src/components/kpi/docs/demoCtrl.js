80f69008938ad6596ad42012123e18858ed218c9
Cody Nicholson <cody.nicholson@visualrevenue.com>
2014-06-20 16:18:23 -0400

    added new directive for ob-kpi-number

--- src/components/kpi/docs/demoCtrl.js
+++ src/components/kpi/docs/demoCtrl.js
@@ -1,19 +1,53 @@
 angular.module('amelia.docs.controllers').controller('kpiEditorialContentController', function($scope) {
-  $scope.kpi_data = {
-    content_hr: {
+  $scope.data = {
+    contentPerHour: {
       today: 651234,
-      last_week: 34535,
-      last_month: 45543,
+      lastWeek: 34535,
+      lastMonth: 45543,
     }
   };
 });
 
 angular.module('amelia.docs.controllers').controller('kpiEditorialContentDemoController', function($scope) {
-  $scope.kpi_data = {
-    content_hr: {
+  $scope.data = {
+    contentPerHour: {
       today: 651234,
-      last_week: 34535,
-      last_month: 45543,
+      lastWeek: 34535,
+      lastMonth: 45543,
     }
   };
+});
+
+angular.module('amelia.docs.controllers').controller('kpiEditorialRecommendDemoController', function($scope, $interval) {
+
+  var recommendKpiData = {
+    recommendationsValue: 3000,
+    currentValue: 15000,
+    expectedValue: 13000,
+    topExpectedValueToday: 12000,
+  };
+
+  var recommendedTotal = recommendKpiData.recommendationsValue + recommendKpiData.currentValue,
+    maxValue;
+
+  if (recommendKpiData.topExpectedValueToday * 3 >= recommendedTotal) {
+    maxValue = recommendKpiData.topExpectedValueToday * 3;
+  } else {
+    maxValue = recommendedTotal;
+  }
+
+  $scope.data = {
+    recommendValue: {
+      value: 12342,
+      label: 'Incremental Content Views /hr',
+      //prefix: '$',
+    },
+    recommendations: 2400,
+    executedRecommendations: 23,
+  };
+
+  $interval(function() {
+    $scope.data.recommendValue.value += 14467;
+  }, 10000);
+
 });
