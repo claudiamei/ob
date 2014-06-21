5a9b62b5bbe599170ba1e0f2dcf9a8bb6925bcf2
Thomas Lavelle <tlavelle@Thomass-MacBook-Air.local>
2014-06-21 11:58:15 -0400

    prettify code...

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -3,102 +3,90 @@
  */
 
 angular.module('amelia-ui.dropdown', [])
-.constant('obDropdownConfig', {
-  menuPosition: {
-    horizontal: 'auto',   // ['auto', 'left', 'right']
-    vertical: 'auto'      // ['auto', 'left', 'right']
-  },
-  toggleEvent: {
-    open: 'mouseenter click',
-    close: 'mouseleave click'
-  }
-})
-.controller('obDropdownController', ['$scope', '$attrs', 'obDropdownConfig', function ($scope, $attrs, obDropdownConfig) {
-  $scope.hPos = angular.isDefined($attrs.hPos) ? $attrs.hPos : obDropdownConfig.menuPosition.horizontal;
-  $scope.vPos = angular.isDefined($attrs.vPos) ? $attrs.vPos : obDropdownConfig.menuPosition.vertical;
-  $scope.call = function (func) {
-    if ($scope[func]) {
-      $scope[func]();
-    } else if ($scope.$parent.logout) {
-      $scope.$parent[func]()
+  .constant('obDropdownConfig', {
+    menuPosition: {
+      horizontal: 'auto', // ['auto', 'left', 'right']
+      vertical: 'auto' // ['auto', 'left', 'right']
+    },
+    toggleEvent: {
+      open: 'mouseenter click',
+      close: 'mouseleave click'
     }
-  };
-}])
-.directive('obDropdown', function () {
-  return {
-    restrict: 'AE',
-    replace: true,
-    scope: {
-      items: '=',
-      title: '=',
-      hover: '='
-    },
-    //   hPos: '@?',  // for menu placement relative to toggle
-    //   vPos: '@?',  // for menu placement relative to toggle
-    // },
-    controller: 'obDropdownController',
-    templateUrl: '../src/components/dropdown/dropdown.html',
-	// link: function(scope, ele, attr){
-	// 	scope.title = attr.title;
-	// },
-  };
-})
-
-// .controller('obDropdownToggleController', ['$scope', '$attrs', 'obDropdownConfig', function ($scope, $attrs, obDropdownConfig) {
-//   $scope.openEvent = angular.isDefined($attrs.hover) ? $attrs.openEvent : obDropdownConfig.toggleEvent.open;
-//   $scope.closeEvent = angular.isDefined($attrs.hover) ? $attrs.closeEvent : obDropdownConfig.toggleEvent.close;
-// }])
-.directive('obDropdownToggle', ['$document', function ($document) {
-  var openElement = null,
-      closeMenu   = angular.noop;
-  return {
-    restrict: 'CA',
-    scope: {
-      hover: '='
-    },
-    // scope: {
-    //   'openEvent': '=?',
-    //   'closeEvent': '=?'
-    // },
-    // controller: 'obDropdownToggleController',
-    link: function(scope, element, attrs) {
-
-      scope.openEvent = (!!scope.hover) ? 'mouseenter click' : 'click';
-      scope.closeEvent = (!!scope.hover) ? 'mouseleave click' : 'click';
-
-        scope.$watch('$location.path', function() {
-          closeMenu();
-        });
-        element.parent().bind(scope.closeEvent, function() {
-          closeMenu();
-        });
-        element.bind(scope.openEvent, function(event) {
-
-          var elementWasOpen = (element === openElement);
-
-          event.preventDefault();
-          event.stopPropagation();
-
-          if (!!openElement) {
-            closeMenu();
-          }
-
-        if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) {
-          element.parent().addClass('open');
-          openElement = element;
-          closeMenu = function (event) {
-            if (event) {
-              event.preventDefault();
-              event.stopPropagation();
-            }
-            $document.unbind('click', closeMenu);
-            element.parent().removeClass('open');
-            closeMenu = angular.noop;
-            openElement = null;
-          };
-          $document.bind('click', closeMenu);
+  })
+  .controller('obDropdownController', ['$scope', '$attrs', 'obDropdownConfig',
+    function($scope, $attrs, obDropdownConfig) {
+      $scope.hPos = angular.isDefined($attrs.hPos) ? $attrs.hPos : obDropdownConfig.menuPosition.horizontal;
+      $scope.vPos = angular.isDefined($attrs.vPos) ? $attrs.vPos : obDropdownConfig.menuPosition.vertical;
+      $scope.call = function(func) {
+        if ($scope[func]) {
+          $scope[func]();
+        } else if ($scope.$parent.logout) {
+          $scope.$parent[func]()
         }
-      });
+      };
     }
-  };
-}]);
+  ])
+  .directive('obDropdown', function() {
+    return {
+      restrict: 'AE',
+      replace: true,
+      scope: {
+        items: '=',
+        title: '=',
+        hover: '='
+      },
+      controller: 'obDropdownController',
+      templateUrl: '../src/components/dropdown/dropdown.html',
+    };
+  })
+  .directive('obDropdownToggle', ['$document',
+    function($document) {
+      var openElement = null,
+        closeMenu = angular.noop;
+      return {
+        restrict: 'CA',
+        scope: {
+          hover: '='
+        },
+        link: function(scope, element, attrs) {
+
+          scope.openEvent = (!!scope.hover) ? 'mouseenter click' : 'click';
+          scope.closeEvent = (!!scope.hover) ? 'mouseleave click' : 'click';
+
+          scope.$watch('$location.path', function() {
+            closeMenu();
+          });
+          element.parent().bind(scope.closeEvent, function() {
+            closeMenu();
+          });
+          element.bind(scope.openEvent, function(event) {
+
+            var elementWasOpen = (element === openElement);
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (!!openElement) {
+              closeMenu();
+            }
+
+            if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) {
+              element.parent().addClass('open');
+              openElement = element;
+              closeMenu = function(event) {
+                if (event) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                $document.unbind('click', closeMenu);
+                element.parent().removeClass('open');
+                closeMenu = angular.noop;
+                openElement = null;
+              };
+              $document.bind('click', closeMenu);
+            }
+          });
+        }
+      };
+    }
+  ]);
--- src/components/kpi/kpi.js
+++ src/components/kpi/kpi.js
@@ -75,9 +75,9 @@ angular.module('amelia-ui.kpi-boxes', ['amelia-ui.utils', 'ui.bootstrap.tooltip'
           newValue,
           oldValue,
           currentTween;
-        
+
         scope.$watch('data', function(newData, oldData) {
-          if(newData === undefined){
+          if (newData === undefined) {
             return;
           }
 
@@ -86,7 +86,7 @@ angular.module('amelia-ui.kpi-boxes', ['amelia-ui.utils', 'ui.bootstrap.tooltip'
             oldValue = oldData;
           } else {
             newValue = newData.value;
-            oldValue = oldData ? oldData.value: 0;
+            oldValue = oldData ? oldData.value : 0;
           }
 
           if (!init) {
