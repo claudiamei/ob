cf0ac18b8e285a0cd05b96959fb7b6dd2032349b
Roy Tsabari <a300600@gmail.com>
2014-06-22 16:29:36 +0300

    move to module pattern, and make the task more clear about what it does

--- tasks/prettify.js
+++ tasks/prettify.js
@@ -1,2 +1,15 @@
 var jsBeautify = require('./staticAnalysis/jsbeautify.js');
-jsBeautify.verifyAndFix();
+
+var path = require('path');
+
+var filesToBeautify = [
+  path.join('docs', '**', '*.js'),
+  path.join('docs', '**', '*.css'),
+  path.join('src', '**', '*.js'),
+  path.join('src', '**', '*.css'),
+  '!' + path.join('docs', 'lib', '**', '*'),
+  '!' + path.join('src', 'lib', '**', '*'),
+  path.join('tasks', '**', '*.js'),
+];
+
+jsBeautify.verifyAndFix(filesToBeautify);
--- tasks/test.js
+++ tasks/test.js
@@ -6,9 +6,19 @@ var jsBeautify = require('./staticAnalysis/jsbeautify.js');
 var path = require('path');
 var Q = require('q');
 
+var filesToBeautify = [
+  path.join('docs', '**', '*.js'),
+  path.join('docs', '**', '*.css'),
+  path.join('src', '**', '*.js'),
+  path.join('src', '**', '*.css'),
+  '!' + path.join('docs', 'lib', '**', '*'),
+  '!' + path.join('src', 'lib', '**', '*'),
+  path.join('tasks', '**', '*.js'),
+];
+
 Q.all([
   src.js.test(),
-  jsBeautify.verifyOnly(),
+  jsBeautify.verifyOnly(filesToBeautify),
   docs.js.test()
 ])
   .then(function() {});
