1c9e712ed1baf18244d3aa308b79bebf8b5ccc26
TJ Lavelle <tjlav5@gmail.com>
2014-06-24 11:33:15 -0400

    HOTFIX: unexpected ")"

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -52,6 +52,48 @@ angular.module('amelia-ui.dropdown', [])
 //   $scope.closeEvent = angular.isDefined($attrs.hover) ? $attrs.closeEvent : obDropdownConfig.toggleEvent.close;
 // }])
 .directive('obDropdownToggle', ['$document',
+  function($document) {
+    var openElement = null,
+      closeMenu = angular.noop;
+    return {
+      restrict: 'CA',
+      scope: {
+        hover: '='
+      },
+      toggleEvent: {
+        open: 'mouseenter click',
+        close: 'mouseleave click'
+      }
+    }
+  }
+])
+  .controller('obDropdownController', ['$scope', '$attrs', 'obDropdownConfig',
+    function($scope, $attrs, obDropdownConfig) {
+      $scope.hPos = angular.isDefined($attrs.hPos) ? $attrs.hPos : obDropdownConfig.menuPosition.horizontal;
+      $scope.vPos = angular.isDefined($attrs.vPos) ? $attrs.vPos : obDropdownConfig.menuPosition.vertical;
+      $scope.call = function(func) {
+        if ($scope[func]) {
+          $scope[func]();
+        } else if ($scope.$parent[func]) {
+          $scope.$parent[func]();
+        }
+      };
+    }
+  ])
+  .directive('obDropdown', function() {
+    return {
+      restrict: 'AE',
+      replace: true,
+      scope: {
+        items: '=',
+        title: '=',
+        hover: '='
+      },
+      controller: 'obDropdownController',
+      templateUrl: '../src/components/dropdown/dropdown.html',
+    };
+  })
+  .directive('obDropdownToggle', ['$document',
     function($document) {
       var openElement = null,
         closeMenu = angular.noop;
@@ -60,85 +102,45 @@ angular.module('amelia-ui.dropdown', [])
         scope: {
           hover: '='
         },
-        toggleEvent: {
-          open: 'mouseenter click',
-          close: 'mouseleave click'
+        link: function(scope, element, attrs) {
+
+          scope.openEvent = (!!scope.hover) ? 'mouseenter click' : 'click';
+          scope.closeEvent = (!!scope.hover) ? 'mouseleave click' : 'click';
+
+          scope.$watch('$location.path', function() {
+            closeMenu();
+          });
+          element.parent().bind(scope.closeEvent, function() {
+            closeMenu();
+          });
+          element.bind(scope.openEvent, function(event) {
+
+            var elementWasOpen = (element === openElement);
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (!!openElement) {
+              closeMenu();
+            }
+
+            if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) {
+              element.parent().addClass('open');
+              openElement = element;
+              closeMenu = function(event) {
+                if (event) {
+                  event.preventDefault();
+                  event.stopPropagation();
+                }
+                $document.unbind('click', closeMenu);
+                element.parent().removeClass('open');
+                closeMenu = angular.noop;
+                openElement = null;
+              };
+              $document.bind('click', closeMenu);
+            }
+          });
         }
-      })
-    .controller('obDropdownController', ['$scope', '$attrs', 'obDropdownConfig',
-      function($scope, $attrs, obDropdownConfig) {
-        $scope.hPos = angular.isDefined($attrs.hPos) ? $attrs.hPos : obDropdownConfig.menuPosition.horizontal;
-        $scope.vPos = angular.isDefined($attrs.vPos) ? $attrs.vPos : obDropdownConfig.menuPosition.vertical;
-        $scope.call = function(func) {
-          if ($scope[func]) {
-            $scope[func]();
-          } else if ($scope.$parent.logout) {
-            $scope.$parent[func]()
-          }
-        };
-      }
-    ])
-    .directive('obDropdown', function() {
-      return {
-        restrict: 'AE',
-        replace: true,
-        scope: {
-          items: '=',
-          title: '=',
-          hover: '='
-        },
-        controller: 'obDropdownController',
-        templateUrl: '../src/components/dropdown/dropdown.html',
       };
-    })
-    .directive('obDropdownToggle', ['$document',
-      function($document) {
-        var openElement = null,
-          closeMenu = angular.noop;
-        return {
-          restrict: 'CA',
-          scope: {
-            hover: '='
-          },
-          link: function(scope, element, attrs) {
-
-            scope.openEvent = (!!scope.hover) ? 'mouseenter click' : 'click';
-            scope.closeEvent = (!!scope.hover) ? 'mouseleave click' : 'click';
-
-            scope.$watch('$location.path', function() {
-              closeMenu();
-            });
-            element.parent().bind(scope.closeEvent, function() {
-              closeMenu();
-            });
-            element.bind(scope.openEvent, function(event) {
-
-              var elementWasOpen = (element === openElement);
-
-              event.preventDefault();
-              event.stopPropagation();
-
-              if (!!openElement) {
-                closeMenu();
-              }
-
-              if (!elementWasOpen && !element.hasClass('disabled') && !element.prop('disabled')) {
-                element.parent().addClass('open');
-                openElement = element;
-                closeMenu = function(event) {
-                  if (event) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                  }
-                  $document.unbind('click', closeMenu);
-                  element.parent().removeClass('open');
-                  closeMenu = angular.noop;
-                  openElement = null;
-                };
-                $document.bind('click', closeMenu);
-              }
-            });
-          }
-        };
-      }
-    ]);
+    }
+  ]);
